e530cc04761fd7aeb4a778790b72b1badc61c855
konda Rajasri <159157914+AKSHITHANAGURI15@users.noreply.github.com>
2024-06-27 11:40:20 +0530

    Add files via upload

---
+++ MergeSort.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class MergeSort
+{
+ public static void main(String args[])
+{
+  Scanner s=new Scanner(System.in);
+   int n=s.nextInt();
+   int a[]=new int[n];
+ for(int i=0;i<n;i++)
+{
+  a[i]=s.nextInt();
+}
+ int m=s.nextInt();
+  int b[]=new int[m];
+for(int j=0;j<m;j++)
+{
+  b[j]=s.nextInt();
+}
+int c[]=new int[m+n];
+int k=0;
+for(int i=0;i<n;i++)
+{
+  c[k]=a[i];
+ k++;
+}
+   for(int i=0;i<m;i++)
+{
+    c[k]=b[i];
+    k++;
+}
+Arrays.sort(c);
+for(int i=0;i<k;i++)
+{
+ System.out.print(c[i]+" ");
+}
+}
+}
+
+
+
